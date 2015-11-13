c3abc40202b76337819777673d58977f5b338e85
drewkerrigan <dkerrigan@basho.com>
2015-11-13 02:18:53 -0800

    adding hex package

---
+++ mix.exs
@@ -0,0 +1,19 @@
+defmodule Hamcrest.Mixfile do
+  use Mix.Project
+
+  @version File.read!("VERSION") |> String.strip
+
+  def project do
+    [app: :hamcrest,
+     version: @version,
+     description: "Erlang port of Hamcrest",
+     package: package]
+  end
+
+  defp package do
+    [files: ~w(ebin/hamcrest.app include priv src test INSTALL LICENCE Makefile NOTES README.markdown rebar.config test.config TODO.md VERSION),
+     maintainers: ["Tim Watson"],
+     licenses: ["Copyright (c) 2010, Tim Watson"],
+     links: %{"GitHub" => "https://github.com/hyperthunk/hamcrest-erlang"}]
+  end
+end
